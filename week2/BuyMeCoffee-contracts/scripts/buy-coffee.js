b8375892d247178b5ff4ebb6d7dcaa70a60b2b86
Gaurav Ajariwal <gauravajariwal.11@gmail.com>
2022-09-17 14:04:42 -0500

    added all the functions to check and withdraw balances

--- week2/BuyMeCoffee-contracts/scripts/buy-coffee.js
+++ week2/BuyMeCoffee-contracts/scripts/buy-coffee.js
@@ -6,11 +6,15 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+// Returns the Ether balance of a given address.
 async function getBalance(address) {
-  const balanceBigInt = await hre.waffle.provider.getBalance(address);
+  // Switch out Waffle to Ethers
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  // const balanceBigInt = await hre.waffle.provider.getBalance(address);
   return hre.ethers.utils.formatEther(balanceBigInt);
 }
 
+// Logs the Ether balances for a list of addresses.
 async function printBalances(addresses) {
   let idx = 0;
   for (const address of addresses) {
@@ -19,6 +23,7 @@ async function printBalances(addresses) {
   }
 }
 
+// Logs the memos stored on-chain from coffee purchases.
 async function printMemos(memos) {
   for (const memo of memos) {
     const timestamp = memo.timestamp;
@@ -30,10 +35,10 @@ async function printMemos(memos) {
 }
 
 async function main() {
-  // Get example accounts
+  // Get the example accounts we'll be working with.
   const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
 
-  // Get contract to deploy
+  // We get the contract to deploy.
   const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
   const buyMeACoffee = await BuyMeACoffee.deploy();
 
@@ -46,26 +51,25 @@ async function main() {
   console.log("== start ==");
   await printBalances(addresses);
 
-  // Buy the owner a few coffess
-  const tip = { value: hre.ethers.utils.parseEther("1") }
-  await buyMeACoffee.connect(tipper).buyCoffee("Carolina", "You're the best", tip);
-  await buyMeACoffee.connect(tipper2).buyCoffee("Vitto", "Amazing teacher", tip);
-  await buyMeACoffee.connect(tipper3).buyCoffee("Kay", "I love my proof of knowledge NFT", tip);
+  // Buy the owner a few coffees.
+  const tip = { value: hre.ethers.utils.parseEther("1") };
+  await buyMeACoffee.connect(tipper).buyCoffee("Sam", "You're the best!", tip);
+  await buyMeACoffee.connect(tipper2).buyCoffee("Rob", "Amazing", tip);
+  await buyMeACoffee.connect(tipper3).buyCoffee("Jim", "I can write better codes", tip);
 
-
-  // Check balances after coffee purchase
+  // Check balances after the coffee purchase.
   console.log("== bought coffee ==");
   await printBalances(addresses);
 
-  // withdraw funds
-  await buyMeACoffee.connect(owner).withDrawTips();
+  // Withdraw.
+  await buyMeACoffee.connect(owner).withdrawTips();
 
-  // Check balances after withdraw tips
-  console.log("== After withdraw tips ==");
+  // Check balances after withdrawal.
+  console.log("== withdrawTips ==");
   await printBalances(addresses);
 
-  // Read all memos
-  console.log("== Memos ==");
+  // Check out the memos.
+  console.log("== memos ==");
   const memos = await buyMeACoffee.getMemos();
   printMemos(memos);
 }
